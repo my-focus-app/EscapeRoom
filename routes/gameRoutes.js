649e6e5467d7cf9e913254fb1d77ed85739210c0
Ewan Ehrensperger <ewan@MacBook-Air-de-Ewan.local>
2025-05-02 09:18:39 +0200

    amélioration de la langue

--- routes/gameRoutes.js
+++ routes/gameRoutes.js
@@ -3,6 +3,7 @@ const express = require('express');
 const path = require('path');
 const dataStore = require('../dataStore');
 const { body, validationResult } = require('express-validator');
+const multer = require('multer');
 
 const router = express.Router();
 
